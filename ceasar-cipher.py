97b5e2c500f2fd06e13c6a564e5be3bd7e08d1e5
imotyashok <imotyashok@luc.edu>
2019-11-18 21:16:01 -0600

    mixed up the charset to increase safety, added a repeat program loop in main

--- ceasar-cipher.py
+++ ceasar-cipher.py
@@ -1,13 +1,14 @@
 # Caesar Cipher
 
+CHAR_SET = "AMNBVCXZLKJHGFDSPOIUYTREWQ `~!@#$%^&*()_+|{}|:\'\"-=[]\;,.<>?/amnbvcxzlkjhgfdspoiuytrewq0192837465"
+
 def encode(plaintext, shift):
     coded_message = ''
-    charSet= "ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{}|:-=[]\;,.<>?/"
     for symbol in plaintext:
-        if symbol in charSet:
-            number = charSet.find(symbol)-charSet.find('A')
-            code_number = (number + shift) % 92
-            code_letter = charSet[code_number + charSet.find('A')]
+        if symbol in CHAR_SET:
+            number = CHAR_SET.find(symbol) - CHAR_SET.find('A')
+            code_number = (number + shift) % 96
+            code_letter = CHAR_SET[code_number + CHAR_SET.find('A')]
             coded_message = coded_message + code_letter
         else:
             coded_message = coded_message + symbol
@@ -15,39 +16,55 @@ def encode(plaintext, shift):
     
 def decode(codetext, shift):
     decoded_message = ''
-    charSet= "ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{}|:-=[]\;,.<>?/"
     for symbol in codetext:
-        if symbol in charSet:
-            number = charSet.find(symbol) - charSet.find('A')
-            decode_number = (number - shift) % 92
-            decode_letter = charSet[decode_number + charSet.find('A')]
+        if symbol in CHAR_SET:
+            number = CHAR_SET.find(symbol) - CHAR_SET.find('A')
+            decode_number = (number - shift) % 96
+            decode_letter = CHAR_SET[decode_number + CHAR_SET.find('A')]
             decoded_message = decoded_message + decode_letter
         else:
             decoded_message = decoded_message + symbol
     return decoded_message
 
+def restart():
+    answer = input(">>> Would you like to run the program again? (y/n) ").lower()
+    if  answer == "y" or answer == "yes":
+        return True
+    elif answer == "n" or answer == "no":
+        print("Thank you for using the Caesar Cipher!")
+        return False
+    else:
+        print("Sorry, that option isn't recognized. ")
+        return False
 
 def main():
+    yes = True
+    while yes:
+        choice = input(">>> Would you like to encode or decode a message? \n"
+              "(Type \"e\" for encode, \"d\" for decode): ").lower()
 
-    choice = input(">>> Would you like to encode or decode a message? \n"
-          "(Type \"e\" for encode, \"d\" for decode): ").lower()
+        if choice == 'e':
+            secretMessage = input(">>> Type the secret message: ")
+            shift = int(input(">>> Type a random positive integer shift value: "))
+            print("(NOTE: if you want to decode the secret message later, you will need"
+                  " to use the same shift value!)")
 
-    if choice == 'e':
-        secretMessage = input(">>> Type the secret message: ")
-        shift = int(input(">>> Type a random positive integer shift value: "))
-        print("(NOTE: if you want to decode the secret message later, you will need"
-              " to use the same shift value!)")
+            codedMessage = encode(secretMessage, shift)
+            print(">>> The encoded message is : ", codedMessage)
 
-        codedMessage = encode(secretMessage, shift)
-        print(">>> The encoded message is : ", codedMessage)
+            yes = restart()
 
-    elif choice == "d":
+        elif choice == "d":
 
-        decodeInput = input(">>> Please type the message you would like to decode: ")
-        decodeShift = int(input(">>> Please enter the shift value associated with the message: "))
-        decodedMessage = decode(decodeInput,decodeShift)
-        print(">>> The decoded message is : ", decodedMessage)
-    else:
-        print("Sorry, the choice \"%s\" is not recognized." % choice)
+            decodeInput = input(">>> Please type the message you would like to decode: ")
+            decodeShift = int(input(">>> Please enter the shift value associated with the message: "))
+            decodedMessage = decode(decodeInput,decodeShift)
+            print(">>> The decoded message is : ", decodedMessage)
+
+            yes = restart()
+
+        else:
+            print("Sorry, the choice \"%s\" is not recognized." % choice)
+            yes = False
 
 main()
